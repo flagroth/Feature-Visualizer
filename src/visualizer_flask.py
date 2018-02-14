93b9ecb723a2d76b0dcc9d5e6374742b0a380e6d
Javier Cano <javier.cano.montero@urjc.es>
2018-02-14 12:02:57 +0100

    Añadido accuracy para visualizaciones lineales
    Para LDA usamos el accuracy del propio método.
    Para PCA y RANDOM utilizamos una SVM con kernel lineal para calcular el accuracy

--- src/visualizer_flask.py
+++ src/visualizer_flask.py
@@ -3,12 +3,15 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 
+from sklearn.metrics import accuracy_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import normalize
 from sklearn.manifold import TSNE
 from sklearn.manifold import MDS
 from sklearn.random_projection import GaussianRandomProjection as GRP
+
+from sklearn.svm import SVC
 
 import pandas as pd
 import numpy as np
@@ -38,7 +41,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Data preparation
-def data_prep(filename=_filename, target=_target, separator=_separator, features=None):
+def data_prep(filename=_filename, target=_target, separator=_separator, train_pct=70, features=None):
     dataset = pd.read_csv("../data/"+filename, sep=separator)
     dataset = dataset.apply(pd.to_numeric, errors='coerce')
     print(dataset.columns)
@@ -53,7 +56,7 @@ def data_prep(filename=_filename, target=_target, separator=_separator, features
     data_columns = dataset.columns
     x = x.apply(pd.to_numeric, errors='coerce')
     x = (x - x.mean()) / (x.max() - x.min())
-    msk = np.random.rand(len(x)) < 0.7
+    msk = np.random.rand(len(x)) < train_pct/100
     train_x = x[msk]
     train_y = y[msk]
     test_x = x[~msk]
@@ -67,8 +70,8 @@ def data_prep(filename=_filename, target=_target, separator=_separator, features
             })
 
 # Plot drawing
-def make_plot(source_points, compo=None, columns=None, method=None):  
-    p = figure(tools = ["pan,wheel_zoom,box_select,lasso_select,reset"], width = 300, height = 300, title=method, sizing_mode='scale_width')
+def make_plot(source_points, compo=None, columns=None, method=None, accuracy=0):  
+    p = figure(tools = ["pan,wheel_zoom,box_select,lasso_select,reset"], width = 300, height = 300, title=method + ". Acc: " + str(accuracy), sizing_mode='scale_width')
     x = 'x_' + method.lower()
     y = 'y_' + method.lower()
     compo_x = 'comp_' + method.lower() + '_x'
@@ -100,9 +103,9 @@ def upload_file():
             filename = secure_filename(file.filename)
             target = request.form.get("target")
             separator = request.form.get("separator")
+            train_pct = request.form.get("train_pct")
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('uploaded_file',
-            return redirect(url_for('features', filename=filename, target=target, separator=separator))
+            return redirect(url_for('features', filename=filename, target=target, separator=separator, train_pct=train_pct))
     return '''
     <!doctype html>
     <link rel=stylesheet type=text/css href="static/cerulean.min.css">
@@ -114,6 +117,7 @@ def upload_file():
                 <p><input type=file name=file>*Only CSV</p>
                 <p>Target column:<input type=text name=target>
                 CSV separator<input type=text name=separator></p>
+                <p>Training percentage:<input type=text name=train_pct></p>
                 <input type=submit value=Upload>
             </form>
         </div>
@@ -121,14 +125,15 @@ def upload_file():
     '''
 
 # Feature selection    
-@app.route('/features?filename=<filename>&target=<target>&separator=<separator>', methods=['GET'])
-def features(filename,target,separator):
-    global _filename,_target, _separator
+@app.route('/features?filename=<filename>&target=<target>&separator=<separator>&train_pct=<train_pct>', methods=['GET'])
+def features(filename,target,separator,train_pct):
+    global _filename,_target, _separator, _train_pct
     _filename=filename
     _target=target
     _separator=separator
+    _train_pct=int(train_pct)
 
-    data=data_prep(_filename,_target,_separator)
+    data=data_prep(_filename,_target,_separator,_train_pct)
     data_columns,train_x,train_y,test_x,test_y = data.values()
     return render_template("visualizer.html", feature_names=train_x.columns, filename=filename, target=target)
 
@@ -138,7 +143,7 @@ def visualize():
     features = request.form.getlist('feature')
     if len(features)==0:
         features=None
-    data=data_prep(_filename, _target, _separator, features)
+    data=data_prep(_filename, _target, _separator, _train_pct, features)
     data_columns,train_x,train_y,test_x,test_y = data.values()
     print(train_x.columns)
     
@@ -146,12 +151,16 @@ def visualize():
     print(vizs)
     
     dataSource=dict()
+    clf = SVC(kernel = 'linear')
     if "PCA" in vizs:
         print("Making PCA")
         pca = PCA(n_components=2)
         X_r_pca = pca.fit(train_x).transform(train_x)
         comp_pca = pca.components_
-        X_r_test = pca.transform(test_x)
+        X_r_pca_test = pca.transform(test_x)
+        clf.fit(X_r_pca,train_y)
+        y_predict_pca = clf.predict(X_r_pca_test)
+        acc_pca = accuracy_score(test_y, y_predict_pca)
         dataSource['x_pca'] = X_r_pca[:,0]
         dataSource['y_pca'] = X_r_pca[:,1]
     
@@ -161,6 +170,7 @@ def visualize():
         X_r_lda = lda.fit(train_x,train_y).transform(train_x)
         comp_lda = lda.coef_
         y_predict_lda = lda.predict(test_x)
+        acc_lda = accuracy_score(test_y, y_predict_lda)
         X_r_lda_test = lda.fit(test_x,y_predict_lda).transform(test_x)
         dataSource['x_lda'] = X_r_lda[:,0]
         dataSource['y_lda'] = X_r_lda[:,1]
@@ -168,8 +178,13 @@ def visualize():
     if "RANDOM" in vizs:
         print("Making RANDOM")
         randomProjection = GRP(n_components=2)
-        X_r_random = randomProjection.fit(train_x,train_y).transform(train_x)
+        randomProjection.fit(train_x,train_y)
+        X_r_random = randomProjection.transform(train_x)
         comp_random = randomProjection.components_
+        X_r_random_test = randomProjection.transform(test_x)
+        clf.fit(X_r_random,train_y)
+        y_predict_random = clf.predict(X_r_random_test)
+        acc_random = accuracy_score(test_y, y_predict_random)       
         dataSource['x_random'] = X_r_random[:,0]
         dataSource['y_random'] = X_r_random[:,1]
     
@@ -178,6 +193,10 @@ def visualize():
         tsne_iter = int(request.form.get('tsne_iter'))
         tsne = TSNE(n_components=2,n_iter=tsne_iter)
         X_r_tsne = tsne.fit_transform(train_x)
+#        X_r_tsne_test = tsne.fit_transform(test_x)
+#        clf.fit(X_r_tsne,train_y)
+#        y_predict_tsne = clf.predict(X_r_tsne_test)
+#        acc_tsne = accuracy_score(test_y, y_predict_tsne)     
         dataSource['x_tsne'] = X_r_tsne[:,0]
         dataSource['y_tsne'] = X_r_tsne[:,1]
 
@@ -203,15 +222,15 @@ def visualize():
     g=[]
     print("Plotting")
     if "PCA" in vizs:
-        g.append(make_plot(source_points, comp_pca, train_x.columns, "PCA"))
+        g.append(make_plot(source_points, comp_pca, train_x.columns, "PCA", acc_pca))
     if "LDA" in vizs:
-        g.append(make_plot(source_points, comp_lda, train_x.columns, "LDA"))
+        g.append(make_plot(source_points, comp_lda, train_x.columns, "LDA", acc_lda))
     if "RANDOM" in vizs:    
-        g.append(make_plot(source_points, comp_random, train_x.columns, "RANDOM"))        
+        g.append(make_plot(source_points, comp_random, train_x.columns, "RANDOM", acc_random))        
     if "TSNE" in vizs:    
-        g.append(make_plot(source_points, method="TSNE"))
+        g.append(make_plot(source_points, method="TSNE", accuracy = 0))
     if "MDS" in vizs:    
-        g.append(make_plot(source_points, method="MDS"))
+        g.append(make_plot(source_points, method="MDS", accuracy = 0))
     
     p = gridplot(g, ncols=2, plot_width=600, plot_height=600, sizing_mode='scale_width')
     script, div = components(p)
